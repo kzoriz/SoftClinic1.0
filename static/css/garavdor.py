5975b62463b491f29605b3d7d36057c0d81ac094
kzoriz <borisoliveira@alu.uern.br>
2022-08-04 17:48:07 -0300

    second commit

---
+++ static/css/garavdor.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import pyautogui
+
+
+SCREEN_SIZE = (1920,1080)
+fourcc = cv2.VideoWrite_fourcc(*"XVID")
+out = cv2.VideoWrite("gravacao.avi", fourcc,20.0,(SCREEN_SIZE))
+
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    out.write(frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+out.release()
