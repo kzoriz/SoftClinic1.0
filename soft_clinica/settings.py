27b2990258c0dab9a5bccf579aada8addd493a9b
kzoriz <borisoliveira@alu.uern.br>
2022-08-29 00:26:19 -0300

    configuração on_heroku**

--- soft_clinica/settings.py
+++ soft_clinica/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#import django_on_heroku
+import django_on_heroku
 
 from pathlib import Path
 import os
@@ -151,7 +151,7 @@ LOGIN_URL = 'login'
 
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 # formato da data
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
