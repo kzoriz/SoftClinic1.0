e4a113bb9fd250b7bc2bcf180b7209dbecf5f690
kzoriz <borisoliveira@alu.uern.br>
2022-12-12 09:44:35 -0300

    variaveis de ambiente

--- soft_clinica/settings.py
+++ soft_clinica/settings.py
@@ -22,15 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG')
-DEBUG = os.environ.get('DEBUG')
-print(DEBUG)
-#ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+DEBUG = os.environ.get('DEBUG') == 'False'
+ALLOWED_HOSTS = ['https://softodon.herokuapp.com']
+
 # Application definition
 
 INSTALLED_APPS = [
