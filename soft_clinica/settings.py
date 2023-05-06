852aea66455c6300226b3328d5ab51d6f585359b
kzoriz <borisoliveira@alu.uern.br>
2023-05-05 23:28:33 -0300

    vercel config

--- soft_clinica/settings.py
+++ soft_clinica/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'soft_clinica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        # 'NAME': os.path.join(BASE_DIR, 'mydb'),
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '',# 8000 is default
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
